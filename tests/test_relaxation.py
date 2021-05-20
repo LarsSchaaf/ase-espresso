bfa8181f4d98af6daf11172ad5e0b362d628c2df
Lars Schaaf <7270605-LarsSchaaf@users.noreply.gitlab.com>
2021-05-20 11:45:52 +0100

    Removing changes: formatting to make comparison easier

--- tests/test_relaxation.py
+++ tests/test_relaxation.py
@@ -1,4 +1,3 @@
-
 # test adapted from Quantum Espresso PWSCF v.5.3.0 (svn rev. 11974)
 #   : PW/examples/example02
 
@@ -16,86 +15,95 @@ def test_relax_co_qe_bfgs(tmpdir):
 
     tmpdir.chdir()
 
-    co = Atoms('CO', [[2.256 * Bohr, 0.0, 0.0], [0.0, 0.0, 0.0]])
+    co = Atoms("CO", [[2.256 * Bohr, 0.0, 0.0], [0.0, 0.0, 0.0]])
     co.set_cell(np.ones(3) * 12.0 * Bohr)
 
-    calc = Espresso(pw=24.0 * Rydberg,
-                    dw=144.0 * Rydberg,
-                    kpts='gamma',
-                    xc='PBE',
-                    calculation='relax',
-                    ion_dynamics='bfgs',
-                    spinpol=False,
-                    outdir='qe_bfgs')
+    calc = Espresso(
+        pw=24.0 * Rydberg,
+        dw=144.0 * Rydberg,
+        kpts="gamma",
+        xc="PBE",
+        calculation="relax",
+        ion_dynamics="bfgs",
+        spinpol=False,
+        outdir="qe_bfgs",
+    )
 
     co.set_calculator(calc)
     calc.calculate(co)
 
-    print('qe bfgs:')
-    print('energy: ', co.get_potential_energy())
-    print('positions: ', co.positions)
-    print('forces: ', co.get_forces())
+    print("qe bfgs:")
+    print("energy: ", co.get_potential_energy())
+    print("positions: ", co.positions)
+    print("forces: ", co.get_forces())
 
 
 def test_relax_co_ase_bfgs(tmpdir):
 
     tmpdir.chdir()
 
-    co = Atoms('CO', [[2.256 * Bohr, 0.0, 0.0], [0.0, 0.0, 0.0]])
+    co = Atoms("CO", [[2.256 * Bohr, 0.0, 0.0], [0.0, 0.0, 0.0]])
     co.set_cell(np.ones(3) * 12.0 * Bohr)
 
-    calc = Espresso(pw=24.0 * Rydberg,
-                    dw=144.0 * Rydberg,
-                    kpts='gamma',
-                    xc='PBE',
-                    calculation='scf',
-                    ion_dynamics=None,
-                    spinpol=False,
-                    outdir='qe_ase_bfgs')
+    calc = Espresso(
+        pw=24.0 * Rydberg,
+        dw=144.0 * Rydberg,
+        kpts="gamma",
+        xc="PBE",
+        calculation="scf",
+        ion_dynamics=None,
+        spinpol=False,
+        outdir="qe_ase_bfgs",
+    )
 
     co.set_calculator(calc)
 
-    minimizer = BFGS(co, logfile='minimizer.log', trajectory='relaxed.traj')
+    minimizer = BFGS(co, logfile="minimizer.log", trajectory="relaxed.traj")
     minimizer.run(fmax=0.01)
 
-    print('ase(scf) bfgs:')
-    print('energy: ', co.get_potential_energy())
-    print('positions: ', co.positions)
-    print('forces: ', co.get_forces())
+    print("ase(scf) bfgs:")
+    print("energy: ", co.get_potential_energy())
+    print("positions: ", co.positions)
+    print("forces: ", co.get_forces())
 
 
 def test_relax_co_ase_interactive_bfgs(tmpdir):
 
     tmpdir.chdir()
 
-    co = Atoms('CO', [[2.256 * Bohr, 0.0, 0.0], [0.0, 0.0, 0.0]])
+    co = Atoms("CO", [[2.256 * Bohr, 0.0, 0.0], [0.0, 0.0, 0.0]])
     co.set_cell(np.ones(3) * 12.0 * Bohr)
 
-    calc = iEspresso(pw=24.0 * Rydberg,
-                     dw=144.0 * Rydberg,
-                     kpts='gamma',
-                     xc='PBE',
-                     calculation='relax',
-                     ion_dynamics='ase3',
-                     spinpol=False,
-                     outdir='qe_ase_interactive_bfgs')
+    calc = iEspresso(
+        pw=24.0 * Rydberg,
+        dw=144.0 * Rydberg,
+        kpts="gamma",
+        xc="PBE",
+        calculation="relax",
+        ion_dynamics="ase3",
+        spinpol=False,
+        outdir="qe_ase_interactive_bfgs",
+    )
 
     co.set_calculator(calc)
 
-    minimizer = BFGS(co, logfile='minimizer.log', trajectory='relaxed.traj')
+    minimizer = BFGS(co, logfile="minimizer.log", trajectory="relaxed.traj")
     minimizer.run(fmax=0.01)
 
     ref_ene = -616.9017404193379
 
-    ref_pos = np.array([[1.19233393e+00, 0.0e+00, 0.00000000e+00],
-                        [1.48996586e-03, 0.00000000e+00, 0.00000000e+00]])
+    ref_pos = np.array(
+        [
+            [1.19233393e00, 0.0e00, 0.00000000e00],
+            [1.48996586e-03, 0.00000000e00, 0.00000000e00],
+        ]
+    )
 
-    ref_for = np.array([[0.00162288, 0.0, 0.0],
-                        [-0.00162288, 0.0, 0.0]])
+    ref_for = np.array([[0.00162288, 0.0, 0.0], [-0.00162288, 0.0, 0.0]])
 
     assert np.allclose(co.get_potential_energy(), ref_ene)
-    #assert np.allclose(co.positions, ref_pos)
-    #assert np.allclose(co.get_forces(), ref_for)
+    # assert np.allclose(co.positions, ref_pos)
+    # assert np.allclose(co.get_forces(), ref_for)
 
     print(co.positions)
     print(co.get_forces())
--- tests/test_siteconfig.py
+++ tests/test_siteconfig.py
@@ -1,4 +1,3 @@
-
 import os
 import pytest
 
@@ -6,38 +5,38 @@ from espresso.siteconfig import SiteConfig
 
 
 def test_pbs_exceptions(tmpdir):
-    'check if an exception is raised when `scratchenv` variable is undefined'
+    "check if an exception is raised when `scratchenv` variable is undefined"
 
     tmpdir.chdir()
-    pytest.raises(OSError, SiteConfig, 'PBS', scratchenv='MYSCRATCH0000')
+    pytest.raises(OSError, SiteConfig, "PBS", scratchenv="MYSCRATCH0000")
 
 
 def test_slurm_exceptions(tmpdir):
-    'check if an exception is raised when `scratchenv` variable is undefined'
+    "check if an exception is raised when `scratchenv` variable is undefined"
 
     tmpdir.chdir()
-    pytest.raises(OSError, SiteConfig, 'SLURM', scratchenv='MYSCRATCH0000')
+    pytest.raises(OSError, SiteConfig, "SLURM", scratchenv="MYSCRATCH0000")
 
 
 def test_pbs_variables(tmpdir):
 
     tmpdir.chdir()
 
-    os.environ['SCRATCH'] = str(tmpdir)
-    os.environ['PBS_JOBID'] = '12345678'
-    os.environ['PBS_O_WORKDIR'] = str(tmpdir)
-    nodefile = tmpdir.join('nodefile')
-    os.environ['PBS_NODEFILE'] = str(nodefile)
+    os.environ["SCRATCH"] = str(tmpdir)
+    os.environ["PBS_JOBID"] = "12345678"
+    os.environ["PBS_O_WORKDIR"] = str(tmpdir)
+    nodefile = tmpdir.join("nodefile")
+    os.environ["PBS_NODEFILE"] = str(nodefile)
 
     # create nodefile for 2 nodes with 16 cpu each
-    nodes = ['node-{0:d}'.format(i) for i in range(2) for _ in range(16)]
-    with nodefile.open('w') as nf:
+    nodes = ["node-{0:d}".format(i) for i in range(2) for _ in range(16)]
+    with nodefile.open("w") as nf:
         for name in nodes:
-            nf.write(name + '\n')
+            nf.write(name + "\n")
 
-    site = SiteConfig('PBS')
+    site = SiteConfig("PBS")
 
-    assert site.jobid == '12345678'
+    assert site.jobid == "12345678"
     assert site.submitdir == str(tmpdir)
     assert site.nprocs == 32
     assert site.hosts == nodes
@@ -50,16 +49,16 @@ def test_slurm_variables(tmpdir):
 
     tmpdir.chdir()
 
-    os.environ['SCRATCH'] = str(tmpdir)
-    os.environ['SLURM_JOB_ID'] = '87654321'
-    os.environ['SUBMITDIR'] = str(tmpdir)
-    os.environ['SLURM_JOB_NUM_NODES'] = '2'
-    os.environ['SLURM_TASKS_PER_NODE'] = '12'
-    os.environ['SLURM_JOB_NODELIST'] = 'node-0,node-1'
+    os.environ["SCRATCH"] = str(tmpdir)
+    os.environ["SLURM_JOB_ID"] = "87654321"
+    os.environ["SUBMITDIR"] = str(tmpdir)
+    os.environ["SLURM_JOB_NUM_NODES"] = "2"
+    os.environ["SLURM_TASKS_PER_NODE"] = "12"
+    os.environ["SLURM_JOB_NODELIST"] = "node-0,node-1"
 
-    site = SiteConfig('SLURM')
+    site = SiteConfig("SLURM")
 
-    assert site.jobid == '87654321'
+    assert site.jobid == "87654321"
     assert site.submitdir == str(tmpdir)
     assert site.nprocs == 24
     assert site.global_scratch == str(tmpdir)
